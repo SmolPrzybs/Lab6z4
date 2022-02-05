47429082c34050e22624f1d6c3f8c1a7c5b1ad22
SmolPrzybs <maciek970@gmail.com>
2022-02-05 23:52:24 +0100

    Dodaj pliki projektów.

---
+++ Lab6z4/Lab6z4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+void potega(int* a) {
+    *a *= *a;
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    int* wsk_a = &a;
+
+    potega(wsk_a);
+    cout << a;
+}
